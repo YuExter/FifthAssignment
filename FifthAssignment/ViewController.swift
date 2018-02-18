0fa79ae9ef20fbec2c6dc963454d4bf94c892fa1
Юля Пономарева <julia@speakapp.com>
2018-02-18 15:38:12 +0300

    Initial Commit

---
+++ FifthAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FifthAssignment
+//
+//  Created by Юля Пономарева on 18.02.2018.
+//  Copyright © 2018 Юля Пономарева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
